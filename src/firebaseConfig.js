ef97751aa9b7fc8f1a611b496bfeedbfe5841916
Dominik Wegrzynowski <szczyntek@gmail.com>
2022-04-22 20:34:40 +0200

    login

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAr_3w7I-smPtrPFqYVAXkTIwrCYxZOkPc",
+    authDomain: "test1-1c82d.firebaseapp.com",
+    projectId: "test1-1c82d",
+    storageBucket: "test1-1c82d.appspot.com",
+    messagingSenderId: "294490596400",
+    appId: "1:294490596400:web:22cee74e9587132cf1b9f5"
+};
